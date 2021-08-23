b3b1dc2cee41dadc09cb857fb9839a6336091ec2
maimai <954392850@qq.com>
2021-08-24 00:59:18 +0800

    创建数据表结构，并进行迁移

---
+++ ideamai/comment/models.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from django.db import models
+
+# Create your models here.
+from blog.models import *
+
+'''
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(os.path.split(rootPath)[0])
+'''
+
+class Comment(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+    )
+
+    target = models.ForeignKey(Post,verbose_name='评论目标')
+    content = models.CharField(max_length=2000,verbose_name='内容')
+    nickname = models.CharField(max_length=50,verbose_name='昵称')
+    website = models.URLField(verbose_name='网站')
+    email = models.EmailField(verbose_name='邮箱')
+    status = models.PositiveIntegerField(default=1,choices=STATUS_ITEMS,verbose_name='状态')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = verbose_name_plural = '评论'
--- ideamai/ideamai/wsgi.py
+++ ideamai/ideamai/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideamai.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideamai.settings")
+profile = os.environ.get('IDEAMAI_PROFILE','develop')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','ideamai.settings.%s' % profile)
+
 
 application = get_wsgi_application()
